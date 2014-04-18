118617c963557f30d55377400d3db33b8a94ab53
Dmitry Khokhlov <khokhlov@targetix.net>
2014-04-19 02:25:17 +0400

    syscalltest: convert tabs to spaces

--- syscalltest/main.c
+++ syscalltest/main.c
@@ -9,73 +9,73 @@
 
 void testProcessCalls() {
 
-	printf( "parent: started\n" );
+    printf( "parent: started\n" );
 
-	int i;
-	for ( i = 0; i < 3; i++ ) {
+    int i;
+    for ( i = 0; i < 3; i++ ) {
 
-		int pid = fork();
-		if ( pid != 0 ) {
+        int pid = fork();
+        if ( pid != 0 ) {
 
-			printf( "parent: child %i forked with PID %i\n", i, pid );
+            printf( "parent: child %i forked with PID %i\n", i, pid );
 
-			int status;
-			waitpid( pid, &status, 0 );
+            int status;
+            waitpid( pid, &status, 0 );
 
-			printf( "parent: child %i exited with status 0x%x\n", i, status );
+            printf( "parent: child %i exited with status 0x%x\n", i, status );
 
-		} else {
+        } else {
 
-			printf( "child %i: started\n", i );
+            printf( "child %i: started\n", i );
 
-			char message[50];
-			sprintf( message, "child %i: echo: executed", i );
+            char message[50];
+            sprintf( message, "child %i: echo: executed", i );
 
-			execl( "/bin/echo", "/bin/echo", message, NULL );
+            execl( "/bin/echo", "/bin/echo", message, NULL );
 
-			printf( "child %i: executing echo failed, error %i\n", i, errno );
-			exit( 1 );
-		}
-	}
-	printf( "parent: exiting\n" );
+            printf( "child %i: executing echo failed, error %i\n", i, errno );
+            exit( 1 );
+        }
+    }
+    printf( "parent: exiting\n" );
 }
 
 void testFileCalls() {
 
-	const int bufferSize = 100;
-	char *filename = "input.txt";
+    const int bufferSize = 100;
+    char *filename = "input.txt";
 
-	puts( "Input one short line of text:" );
+    puts( "Input one short line of text:" );
 
-	char input[ bufferSize ];
-	fgets( input, bufferSize, stdin );
+    char input[ bufferSize ];
+    fgets( input, bufferSize, stdin );
 
-	int fd = open( filename, O_CREAT | O_TRUNC | O_WRONLY, 0644 );
-	write( fd, input, strlen( input ) );
-	close( fd );
+    int fd = open( filename, O_CREAT | O_TRUNC | O_WRONLY, 0644 );
+    write( fd, input, strlen( input ) );
+    close( fd );
 
-	printf( "Your text is saved to '%s'.\n", filename );
+    printf( "Your text is saved to '%s'.\n", filename );
 
-	char output[ bufferSize ];
+    char output[ bufferSize ];
 
-	fd = open( filename, O_RDONLY );
-	int length = read( fd, output, bufferSize );
-	close( fd );
+    fd = open( filename, O_RDONLY );
+    int length = read( fd, output, bufferSize );
+    close( fd );
 
-	puts( "Here what we can read from this file:" );
+    puts( "Here what we can read from this file:" );
 
-	output[ length ] = 0;
-	fputs( output, stdout );
+    output[ length ] = 0;
+    fputs( output, stdout );
 
-	struct stat info;
-	stat( filename, &info );
+    struct stat info;
+    stat( filename, &info );
 
-	printf( "This file has UID %u, GID %u and size %li.\n", info.st_uid, info.st_gid, info.st_size );
+    printf( "This file has UID %u, GID %u and size %li.\n", info.st_uid, info.st_gid, info.st_size );
 }
 
 int main( int argc, char **argv )
 {
-	//testProcessCalls();
-	testFileCalls();
-	return 0;
+    //testProcessCalls();
+    testFileCalls();
+    return 0;
 }
