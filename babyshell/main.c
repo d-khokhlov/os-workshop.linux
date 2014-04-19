133481dff99a96c94249bbc5f0732827cecd73ba
Dmitry Khokhlov <khokhlov@targetix.net>
2014-04-19 04:53:47 +0400

    babyshell: add command parameters handling

--- babyshell/main.c
+++ babyshell/main.c
@@ -8,24 +8,50 @@
 int main( int argc, char **argv )
 {
     char *prompt = "> ";
-    char buffer[ BUFFER_SIZE ] = "";
+    char *spaces = " \t\n\r\v\f";
+    char input[ BUFFER_SIZE ] = "";
+    char *parameters[ BUFFER_SIZE ];
 
+    // Main loop
     while ( 1 ) {
 
+        // Print prompt
         printf( "%s", prompt );
 
-        fgets( buffer, BUFFER_SIZE, stdin );
-        buffer[ strlen( buffer ) - 1 ] = 0;
+        // Read command line from user
+        fgets( input, BUFFER_SIZE, stdin );
 
-        if ( strcmp( buffer, "exit" ) == 0 ) {
+        // Split command line into parameters
+        int i = 0;
+        parameters[ 0 ] = strtok( input, spaces );
+        while( parameters[ i ] != NULL ) {
+            i++;
+            parameters[ i ] = strtok( NULL, spaces );
+        }
+
+        // Handle empty command line and 'exit' command
+        if ( parameters[ 0 ] == NULL ) {
+            continue;
+        } else if ( strcmp( parameters[ 0 ], "exit" ) == 0 ) {
             break;
         }
 
+        // Fork current process to perform command execution
         int pid = fork();
         if ( pid != 0 ) {
+
+            // Parent process waits until child ends
             waitpid( pid, NULL, 0 );
+
         } else {
-            execlp( buffer, buffer, NULL );
+
+            // Execute command in child process
+            execvp( parameters[ 0 ], parameters );
+
+            // We can reach here only if command executable
+            // wasn't load into this process, assume bad command
+            puts( "error: unknown command");
+            return 1;
         }
     }
 
