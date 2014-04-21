f46dbb6bc6cc8b5746e83114a45654396c59154b
Dmitry Khokhlov <khokhlov@targetix.net>
2014-04-22 01:22:11 +0400

    syscalltest: implement FS and other calls

--- syscalltest/main.c
+++ syscalltest/main.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <string.h>
+#include <signal.h>
+#include <time.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
 
@@ -31,7 +33,7 @@ void testProcessCalls() {
             char message[50];
             sprintf( message, "child %i: echo: executed", i );
 
-            execl( "/bin/echo", "/bin/echo", message, NULL );
+            execlp( "echo", "echo", message, NULL );
 
             printf( "child %i: executing echo failed, error %i\n", i, errno );
             exit( 1 );
@@ -57,15 +59,20 @@ void testFileCalls() {
     printf( "Your text is saved to '%s'.\n", filename );
 
     char output[ bufferSize ];
-
     fd = open( filename, O_RDONLY );
     int length = read( fd, output, bufferSize );
-    close( fd );
 
     puts( "Here what we can read from this file:" );
-
     output[ length ] = 0;
     fputs( output, stdout );
+
+    puts( "Let's try to rewind and read the first character again:" );
+    lseek( fd, 0, SEEK_SET );
+    read( fd, output, 1 );
+    output[ 1 ] = 0;
+    puts( output );
+
+    close( fd );
 
     struct stat info;
     stat( filename, &info );
@@ -73,9 +80,106 @@ void testFileCalls() {
     printf( "This file has UID %u, GID %u and size %li.\n", info.st_uid, info.st_gid, info.st_size );
 }
 
+void ls() {
+    if ( fork() == 0 ) {
+        execlp( "ls", "ls", "-lAh", NULL );
+        exit( 1 );
+    }
+    wait( NULL );
+    printf( "\n" );
+}
+
+void testFsCalls() {
+
+    char *dirname = "some-test-dir";
+
+    printf( "Let's create directory '%s'.\n", dirname );
+    mkdir( dirname, 0777 );
+
+    printf( "Here what we have:\n" );
+    ls();
+
+    printf( "Now let's delete this directory.\n" );
+    rmdir( dirname );
+
+    printf( "And here what we have now:\n" );
+    ls();
+
+    char *filename = "some-test-file";
+    char *linkname = "some-test-link";
+
+    printf( "Now lets create file '%s'.\n", filename );
+    close( open( filename, O_CREAT | O_TRUNC | O_WRONLY, 0644 ) );
+
+    printf( "And link it with name '%s'.\n", linkname );
+    link( filename, linkname );
+
+    printf( "We got this (pay attention to the second column):\n" );
+    ls();
+
+    printf( "Let's unlink original name first:\n" );
+    unlink( filename );
+    ls();
+
+    printf( "And now unlink second (link) name:\n" );
+    unlink( linkname );
+    ls();
+}
+
+void testOtherCalls() {
+
+    char *filename = "temp-file-for-chmod-test";
+
+    printf( "Let's go to '/tmp':\n" );
+    chdir( "/tmp" );
+    ls();
+
+    printf( "Now let's create file '%s' here with minimal permissions:\n", filename );
+    close( open( filename, O_CREAT | O_TRUNC | O_WRONLY, 0400 ) );
+    ls();
+
+    printf( "And promote it's permissions:\n" );
+    chmod( filename, 0777 );
+    ls();
+
+    unlink( filename );
+
+    printf( "And now let's fork child process\nthat will print current time every second.\nPress ENTER when ready.\nTo kill child process press ENTER again.\n");
+    getchar();
+
+    int pid = fork();
+    if ( pid == 0 ) {
+        while ( 1 ) {
+            printf( "Current timestamp: %li\n", time( NULL ) );
+            sleep( 1 );
+        }
+        exit( 0 );
+    }
+
+    getchar();
+    kill( pid, SIGKILL );
+    printf( "Child process killed.\n" );
+}
+
 int main( int argc, char **argv )
 {
-    //testProcessCalls();
+    printf( "\nTesting process management calls\n\n" );
+    testProcessCalls();
+    printf( "\n(press ENTER to continue)\n" );
+    getchar();
+
+    printf( "\nTesting files IO calls\n\n" );
     testFileCalls();
+    printf( "\n(press ENTER to continue)\n" );
+    getchar();
+
+    printf( "\nTesting filesystem management calls\n\n" );
+    testFsCalls();
+    printf( "(press ENTER to continue)\n" );
+    getchar();
+
+    printf( "\nTesting other system calls\n\n" );
+    testOtherCalls();
+
     return 0;
 }
